56fdd056140a62617b3e3a4ea303a1232ba4557c
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-07-27 16:18:02 -0300

    getting started

---
+++ gettingStarted.js
@@ -0,0 +1,15 @@
+
+
+var//https://graphql.org/code/#javascript { graphql, buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
